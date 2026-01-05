1e498985bbfd0ce11954dc87e346fb40f10b3861
ice <bing0002@sina.com>
2026-01-05 15:05:51 +0800

    调整4大文字样式的颜色

--- theme.js
+++ theme.js
@@ -145,6 +145,45 @@ function topbarfixedButton() {
   );
 }
 
+/* 顶栏添加文档树按钮 from QYL*/
+(function () {
+  const isMobile = !!window.siyuan.mobile; //在平板上好像有问题
+  if (isMobile) addTopbarTreeButton();
+})();
+function addTopbarTreeButton() {
+  const treeBtn = document.getElementById("topbarTreeBtn");
+  if (!treeBtn) {
+    const toolbarVIP = document.getElementById("toolbarVIP");
+    const windowControls = document.getElementById("windowControls");
+    const newBtn = document.createElement("div");
+    newBtn.id = "topbarTreeBtn";
+    newBtn.className = "toolbar__item ariaLabel";
+    newBtn.style.width = "23.5px";
+    newBtn.style.height = "23.5px";
+    newBtn.innerHTML = `<svg><use xlink:href="#iconFiles"></use></svg>`;
+    newBtn.ariaLabel = "<span style='white-space:pre'>Doc Tree Alt+1 </span>";
+    newBtn.style.userSelect = "none";
+    const handleToolbarClick = () => {
+      const settingsWindow = document.getElementById("QYLsettings-window");
+      settingsWindow ? closeSettingsWindow() : createSettingsWindow();
+    };
+    const parentElement = toolbarVIP?.parentElement || windowControls?.parentElement;
+    if (parentElement) {
+      parentElement.insertBefore(newBtn, toolbarVIP || windowControls);
+      // newBtn.addEventListener("click", handleToolbarClick);
+      newBtn.addEventListener("click", () => {
+        // 模拟点击文档树按钮
+        const docTreeBtn = document.getElementsByClassName("dock__item ariaLabel")[0];
+        if (docTreeBtn) {
+          docTreeBtn.click();
+        } else {
+          alert("未找到文档树按钮，请检查选择器");
+        }
+      });
+    }
+  }
+}
+
 /* -------自动展开悬浮窗折叠列表,展开搜索条目折叠列表,聚焦单独列表 from Savor ---------- */
 function autoOpenList() {
   setInterval(() => {
