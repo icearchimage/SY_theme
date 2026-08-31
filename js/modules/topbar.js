e258d1931def216d8d7476acec37ad0b6dbbc82f
ice <bing0002@sina.com>
2026-08-31 19:42:04 +0800

    手机端无topbar后去掉相关代码

--- js/modules/topbar.js
+++ js/modules/topbar.js
@@ -1,79 +1,16 @@
 /* ----------------------------- 顶栏悬浮 from Savor ---------------------------- */
 
 import {
-  isOfficialMobileLayout,
-  isLikelyMobileBrowser,
   isTabletDevice,
-  logDeviceDetection,
   shouldDisableTopbarFloat,
   shouldUseMobileThemeLayout,
 } from "./device.js";
 
 const TOOLBAR_SHOW_CLASS = "my-theme--toolbar-show";
 
-function topbarfixedButton() {
-  notionThemeToolplusAddButton(
-    "topBar",
-    "toolbar__item b3-tooltips b3-tooltips__sw",
-    "隐藏顶栏",
-    "/appearance/themes/Savor/img/topbar2.svg",
-    "/appearance/themes/Savor/img/topbar.svg",
-    () => {
-      loadStyle("/appearance/themes/Savor/style/topbar/top-fixed.css", "topbar隐藏").setAttribute(
-        "topBarcss",
-        "topbar隐藏"
-      );
-    },
-    () => {
-      document.getElementById("topbar隐藏").remove();
-    },
-    true
-  );
-}
-
-/* 顶栏添加文档树按钮 from QYL*/
-function addTopbarTreeButton() {
-  const treeBtn = document.getElementById("topbarTreeBtn");
-  if (!treeBtn) {
-    const toolbarVIP = document.getElementById("toolbarVIP");
-    const windowControls = document.getElementById("windowControls");
-    const newBtn = document.createElement("div");
-    newBtn.id = "topbarTreeBtn";
-    newBtn.className = "toolbar__item ariaLabel";
-    newBtn.style.width = "23.5px";
-    newBtn.style.height = "23.5px";
-    newBtn.innerHTML = `<svg><use xlink:href="#iconFiles"></use></svg>`;
-    newBtn.ariaLabel = "<span style='white-space:pre'>Doc Tree Alt+1 </span>";
-    newBtn.style.userSelect = "none";
-    const handleToolbarClick = () => {
-      const settingsWindow = document.getElementById("QYLsettings-window");
-      settingsWindow ? closeSettingsWindow() : createSettingsWindow();
-    };
-    const parentElement = toolbarVIP?.parentElement || windowControls?.parentElement;
-    if (parentElement) {
-      parentElement.insertBefore(newBtn, toolbarVIP || windowControls);
-      // newBtn.addEventListener("click", handleToolbarClick);
-      newBtn.addEventListener("click", () => {
-        // 模拟点击文档树按钮
-        const docTreeBtn = document.getElementsByClassName("dock__item ariaLabel")[0];
-        if (docTreeBtn) {
-          docTreeBtn.click();
-        } else {
-          alert("未找到文档树按钮，请检查选择器");
-        }
-      });
-    }
-  }
-}
-
 function initDeviceBodyClass() {
   if (shouldUseMobileThemeLayout()) {
     document.body.classList.add("body--mobile");
-    document.body.classList.toggle("body--mobile-browser", isLikelyMobileBrowser());
-
-    if (navigator.platform.toUpperCase().indexOf("MAC") > -1) {
-      document.body.classList.add("body--mac");
-    }
     return;
   }
 
@@ -153,10 +90,5 @@ function initFloatingToolbarAltToggle() {
 
 export const initTopbar = () => {
   initDeviceBodyClass();
-  // logDeviceDetection();
   initFloatingToolbarAltToggle();
-
-  if (isOfficialMobileLayout()) {
-    addTopbarTreeButton();
-  }
 };
